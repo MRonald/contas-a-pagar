c20f73434713677805dec12a6c1f192d963a3483
MRonald <ronaldmichael918@gmail.com>
2021-04-24 23:47:58 -0300

    Estilizando o ListUsers | Trabalhando responsividade do elemento User

--- src/components/ListUsers.js
+++ src/components/ListUsers.js
@@ -3,18 +3,20 @@ import axios from 'axios';
 
 import User from './User';
 
+import styles from '../style/ListUsers.module.css';
+
 export default function ListUsers() {
     const [users, setUsers] = useState([]);
 
     useEffect(() => {
         axios.get('https://www.mocky.io/v2/5d531c4f2e0000620081ddce').then(
             response => setUsers(response.data)
-        )
+        );
     }, []);
 
     return (
         <div>
-            <ul>
+            <ul className={styles.listUsers}>
                 {users.map(
                     user => (
                         <li key={user.id}>
