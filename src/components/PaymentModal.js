a9f58611e56a1e1f99a70d1da4284f9a5da47680
MRonald <ronaldmichael918@gmail.com>
2021-04-30 21:50:10 -0300

    Mostrando os cartões dinamicamente | Atualizando estilo do PaymentModal

--- src/components/PaymentModal.js
+++ src/components/PaymentModal.js
@@ -3,6 +3,21 @@ import React from 'react';
 import styles from '../style/PaymentModal.module.css';
 
 export default function PaymentModal(props) {
+    const cards = [
+        // valid card
+        {
+          card_number: '1111111111111111',
+          cvv: 789,
+          expiry_date: '01/18',
+        },
+        // invalid card
+        {
+          card_number: '4111111111111234',
+          cvv: 123,
+          expiry_date: '01/20',
+        },
+    ];
+
     return (
         <div className={styles.paymentModalWrapper}>
             <header>
@@ -12,11 +27,15 @@ export default function PaymentModal(props) {
                 <span className={styles.btnCloseModal} onClick={props.functionCloseModal}>X</span>
             </header>
             <main>
-                <input type="text"/>
+                <input type="text" placeholder="R$ 0,00"/>
                 <select>
-                    <option>Cartão com o final 0123</option>
-                    <option>2</option>
-                    <option>3</option>
+                    {cards.map(
+                        card => (
+                            <option>
+                                Cartão com o final {card.card_number.substr(-4)}
+                            </option>
+                        )
+                    )}
                 </select>
                 <button>Pagar</button>
             </main>
